181fbb4d4abff48c5877c6f5f771021ab633822f
faizanshafiq <faizanshafiq123@gmail.com>
2017-09-13 05:23:27 -0700

    I have uploaded my .Net practice project

---
+++ LearnDotnet/LearnDotnet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some thing comes here");
+            int New;
+            New = 1000;
+            Console.WriteLine("{0.00}", New);
+            string[10] l1 = new string[10]();
+            List<string> l= new List<string>();
+            l.Add("dcdd");
+            Console.Read();
+        }
+    }
+}
